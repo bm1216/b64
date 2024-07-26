ae981ecf990efa3a0de407e59554c7c222c0bc71
Barun Mishra <barun.mishra@thehutgroup.com>
2024-07-26 09:08:43 +0100

    add more test

--- test_encode.py
+++ test_encode.py
@@ -3,6 +3,10 @@ from encode import convert_to_b64
 
 
 def test_convert_to_base64():
-    data = "hello"
-    result = convert_to_b64(data)
-    assert (result) == "aGVsbG8="
+    test_cases = {
+        "hello": "aGVsbG8=",
+        "hi there my name is": "aGkgdGhlcmUgbXkgbmFtZSBpcw==",
+    }
+    for key, value in test_cases.items():
+        result = convert_to_b64(key)
+        assert (result) == value
